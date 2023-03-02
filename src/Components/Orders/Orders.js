53ad8ef4f86bd358dfccf7ce445a1105ea251e19
shahrukh khan <shahrukh.khan072@gmail.com>
2023-03-02 13:05:52 +0530

    react e-commerce app

---
+++ src/Components/Orders/Orders.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Orders = () => {
+   
+    return (
+        <div>
+            <h1>This is orders: </h1>
+            
+        </div>
+    );
+};
+
+export default Orders;
